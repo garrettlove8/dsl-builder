7262e847f707d21ea9d64bf01720bea75846b388
Garrett Love <4937384-garrettlove@users.noreply.gitlab.com>
2021-07-27 20:59:24 -0400

    add ability to read command line args

--- src/main.c
+++ src/main.c
@@ -1,29 +1,7 @@
 #include <stdio.h>
-// #include <stdarg.h>
+#include <stdarg.h>
 
-// int myPrintf(const char *format, ...) {
-// 	va_list arg;
-// 	int done;
- 
-//   	va_start (arg, format);
-// 	done = vfprintf (stdout, format, arg);
-// 	va_end (arg);
-
-// 	return done;
-
-	// printf("stdin: %s", "myPrintf");
-	// return 0;
-// }
-
-int printConsole(char text) {
-	putc(text, stdout);
-	return 0;
-}
-
-int main(void) {
-	// puts("Hello, this is the CLI");
-	// printf("stdin: %s", "test input");
-	// myPrintf("stdin: %s", "test input");
-	printConsole(71);
+int main(int argc, char *argv[]) {
+	printf("stdin: %s\n", argv[1]);
 	return 0;
 }
