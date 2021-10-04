19105f346aab69fa8d75e1b0b196ce0a7f00773c
Garrett Love <4937384-garrettlove@users.noreply.gitlab.com>
2021-10-04 07:54:11 -0400

    Add token struct declarations

--- src/tokens.h
+++ src/tokens.h
@@ -1,14 +1,15 @@
-// token_t is extended by each mdoel in the user's program.
-// token_t provides a default behavior function which the
-// then overrides to provide the desired funcionality of a
-// given token, which token_t calls on their behalf instead
-// if its default behavior function.
-typedef struct {
-	char lexicalUnit;
-} token_t;
+#ifndef TOKEN_H
+#define TOKEN_H
+
 
 typedef struct {
-	int (*funcOne) (void*, ...);
-	int (*funcTwo) (void* objPtr);
-	int (*funcThree) (void* objPtr);
-} symbols_t;
+	char* lexUnit;
+	void* (*fn)(void*);
+	statement_t* statement;
+	token_t* previous;
+	token_t* next;
+} token_t;
+
+void* execute(token_t*);
+
+#endif
