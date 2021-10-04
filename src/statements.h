bd6d928c7f6b45046a1bbb5b817b056f055e48a8
Garrett Love <4937384-garrettlove@users.noreply.gitlab.com>
2021-10-04 08:17:14 -0400

    Update statements API and add comments for documentation

--- src/statements.h
+++ src/statements.h
@@ -1,13 +1,29 @@
 #ifndef STATEMENTS_H
 #define STATEMENTS_H
 
+// statement_t is a single statement within a larger DSL script.
 typedef struct {
-	char text[250];
+	// full is a pointer to the complete string of text making up the statement.
+	char* full;
+
+	// obj is a pointer to the statements memory region.
+	// As the statement is executed, this is where the results will be stored.
 	void* obj;
 } statement_t;
 
-statement_t NewStatement();
+// newStatement constructs a statement object - inluding the allocation of
+// its memory region.
+statement_t newStatement(char*);
 
-void DeleteStatement();
+// deleteStatement destructs a statement - including its memory region.
+void deleteStatement(statement_t*);
+
+// parseStatement creates the token structures found in the statement base
+// on the lexical units.
+void parseStatement(statement_t*);
+
+// executeStatement runs the statement's logic by running each tokens function
+// keeping track of the effects of each on the statement's memory region.
+void executeStatement(statement_t*);
 
 #endif
