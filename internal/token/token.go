c9db532c3d118e0776f52dc52951483eb7a74007
Garrett Love <4937384-garrettlove@users.noreply.gitlab.com>
2021-10-17 10:16:04 -0400

    NewToken complete

--- internal/token/token.go
+++ internal/token/token.go
@@ -1,9 +1,11 @@
 package token
 
+import "dsl-builder/internal/statement"
+
 // token_t represents a single lexical unit (token) in a DSL statement.
 type token struct {
-	// lexUnit is a string representation of the token's lexical unit.
-	lexUnit string
+	// literal is a string representation of the token's lexical unit.
+	literal string
 
 	// fn is the function the carries out the instructions of a speicific
 	// token.
@@ -19,6 +21,24 @@ type token struct {
 	next *token
 }
 
-// execute calls the function located at token_t->fn.
+// NewToken is a constructor function that returns a pointer to a new toek struct instance.
+// Currently, there is not much of a difference between using this function and manually
+// creating a new token struct instance. However, this function has been added because
+// It is expected that there will be a difference in the future (ex. Validation or some sort).
+func NewToken(literal string, fn func(...interface{}) interface{}, statement *statement.Statement, previous *token, next *token) (*token, error) {
+	// TODO: Instead of taking in a statement it shoudl take in an interface of some kind.
+
+	newToke := token{
+		literal:   literal,
+		fn:        fn,
+		statement: statement,
+		previous:  previous,
+		next:      next,
+	}
+
+	return &newToke, nil
+}
+
+// Execute calls the function located at token_t->fn.
 // This is how a token's logic is executed by a statement.
-func execute(token *token) {}
+func Execute(token *token) {}
